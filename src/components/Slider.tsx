187ab06dba1b3bf4c1f3e6711e5795a40bfce558
Prakash-Dantani <dantaniprakash08@gmail.com>
2025-04-04 18:06:22 +0530

    Developing Slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,23 +1,35 @@
 import SimpleImageSlider from "react-simple-image-slider";
 
+import blackSand from "../assets/images/black_sand.jpeg";
+import diomandAqua from "../assets/images/diomand-aqua-wp.jpg";
+import diomandBlack from "../assets/images/diomand-black-wp.jpg";
+import diomandBlue from "../assets/images/diomand-blue-wp.jpg";
+
 const Slider = () => {
   const images = [
-    { url: "/assets/images/black_sand.jpeg" },
-    { url: "/assets/images/diomand-aqua-wp.jpg" },
-    { url: "/assets/images/diomand-black-wp.jpg" },
-    { url: "/assets/images/diomand-blue-wp.jpg" },
+    { url: blackSand },
+    { url: diomandAqua },
+    { url: diomandBlack },
+    { url: diomandBlue },
+    { url: diomandAqua },
   ];
 
   return (
-    <div className="pt-12 md:pt-8 text-lg text-center font-bold">
+    <div className="pt-20 md:pt-8 text-lg text-center font-bold">
       <p>Trusted by the world’s most innovative teams</p>
-      <div>
+      <div className="justify-center w-full h-[20vh] pt-10" >
         <SimpleImageSlider
-          width={896}
-          height={504}
+          width={'80vw'}
+          height={'30vh'}
           images={images}
           showBullets={true}
           showNavs={true}
+          useGPURender={true}
+          loop={true}
+          autoPlay={true}
+          slideDuration={0.7}
+          autoPlayDelay={2.5}
+          
         />
       </div>
     </div>
