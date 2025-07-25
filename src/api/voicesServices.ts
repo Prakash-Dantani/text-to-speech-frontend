1eb448ee01d0ee2ac6dfb0f5e470d1e25450d302
Prakash Dantani <110656797+Prakash-Dantani@users.noreply.github.com>
2025-07-25 11:25:54 +0530

    Update voicesServices.ts

--- src/api/voicesServices.ts
+++ src/api/voicesServices.ts
@@ -1,6 +1,6 @@
 // src/api/voiceService.ts
 
-import ApiClient from "../services/ApiClient";
+import ApiClient from "../services/apiClient";
 
 export const getVoices = async (page: number) => {
   const response = await ApiClient.get(`/voices?page=${page}&limit=5`);
