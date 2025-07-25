1e4fae3199aa129f827ffceb806ac2f0a5acdf30
Prakash Dantani <110656797+Prakash-Dantani@users.noreply.github.com>
2025-07-25 11:51:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173, // use Render's PORT if available
+    allowedHosts: ['text-to-speech-frontend-lywk.onrender.com']
   },
 });
