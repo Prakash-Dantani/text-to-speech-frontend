5e9f4f7c442e6d386df739be62d5d13fab5ef7ff
Prakash Dantani <110656797+Prakash-Dantani@users.noreply.github.com>
2025-07-31 08:56:35 +0530

    Update ApiClient.tsx

--- src/services/ApiClient.tsx
+++ src/services/ApiClient.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const ApiClient = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "https://tts-backend-ql5t.onrender.com/api/",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
